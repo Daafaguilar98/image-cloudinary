657e59fa0bc32e6d46c9d6c1307f669babaa5008
Diego Aguilar <daafaguilar98@gmail.com>
2019-04-05 11:41:33 -0500

    add method to plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var Component = require("./index.vue").default;
+var Method = require("./cloudinary_setting.js");
 
 var modules = {};
 modules.install = function(Vue) {
   Vue.component("ImageCloudinary", Component);
+  Vue.prototype.$ImageCloudinary = Method;
 };
 
 export default modules;
